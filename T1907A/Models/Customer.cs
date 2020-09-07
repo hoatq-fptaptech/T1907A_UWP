b0bec9f11ff6fe227af635f4443e87cde602d32c
hoatq-trunganhedukids <hoatq@trunganhedukids.com>
2020-09-07 17:07:57 +0700

    Add project files.

---
+++ T1907A/Models/Customer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1907A.Models
+{
+    public class Customer
+    {
+        private string fullName;
+        private string telePhone;
+        private int age;
+        private DateTime birthday;
+
+        public Customer(string fullName, string telePhone, int age, DateTime birthday)
+        {
+            FullName = fullName;
+            TelePhone = telePhone;
+            Age = age;
+            Birthday = birthday;
+        }
+
+        public string FullName
+        {
+            get => fullName;
+            set => fullName = value;
+        }
+
+        public string TelePhone
+        {
+            get => telePhone;
+            set => telePhone = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public DateTime Birthday
+        {
+            get => birthday;
+            set => birthday = value;
+        }
+
+        public string ToString()
+        {
+            return FullName + "--" + TelePhone +
+                "--" + Age + "--" + Birthday.ToString()+"\n";
+        }
+    }
+}
---
+++ T1907A/Pages/CustomerPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using T1907A.Models;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace T1907A.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CustomerPage : Page
+    {
+       // public static List<Customer> customers = new List<Customer>();
+
+        public CustomerPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            Customer c = new Customer(FullName.Text, Telephone.Text,
+                Convert.ToInt32(Age.Text), Birthday.Date.DateTime);
+            //customers.Add(c);
+            LV.Items.Add(c.ToString());
+        }
+
+    }
+}
