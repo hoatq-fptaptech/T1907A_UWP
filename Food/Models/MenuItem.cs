9f66b22aea70d379f40771b23468993432b9d48b
hoatq-fptaptech <65893435+hoatq-fptaptech@users.noreply.github.com>
2020-09-14 16:33:57 +0700

    add Food

---
+++ Food/Models/MenuItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food.Models
+{
+    class MenuItem
+    {
+        private string name;
+        private string code;
+        private string icon;
+
+        public MenuItem(string name, string code, string icon)
+        {
+            Name = name;
+            Code = code;
+            Icon = icon;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Code { get => code; set => code = value; }
+        public string Icon { get => icon; set => icon = value; }
+    }
+}
